74d9530c0221e4902710610956e1feaf9a63934e
James <zambranojamesj@gmail.com>
2022-07-06 14:23:39 -0300

    add db connection

---
+++ controller/db/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params, callback) => pool.query(text, params, callback),
+};
